5dd0e10b49c9818589c8ef9f70240a94e55c630a
Asier Barrasa <asierjarrillero@gmail.com>
2018-12-10 10:07:42 +0100

    match code clean

--- code/partido.py
+++ code/partido.py
@@ -3,32 +3,48 @@ import requests
 import lcddriver
 import time
 from bs4 import BeautifulSoup
+import sys
 
 display = lcddriver.lcd()
 
+# Search for digits on an string
+def searchForDigits(s):
+   return int(''.join(list(filter(str.isdigit, s))))
+
 try:
     while True:
-       #url = input("https://www.resultados-futbol.com/partido/X/X")
-        url = "https://www.resultados-futbol.com/partido/Juventus-Fc/Internazionale"
+        # Get the url of the match as an argument
+        url = sys.argv[1]
         headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
         response = requests.get(url, headers=headers)
         soup = BeautifulSoup(response.text, "lxml")
 
-        mydivs = soup.findAll("span", {"class": "claseR"})
+        score = soup.findAll("span", {"class": "claseR"})
         timer = soup.findAll("span", {"class": "jor-live"})
-        local = mydivs[0].text
-        visitante = mydivs[1].text
-        timer_txt = timer[0].text
-        t = int(''.join(list(filter(str.isdigit, timer_txt))))
         name = soup.findAll("b", {"itemprop": "name"})
-        x = local + "-" + visitante + "     time: " + str(t)
-        y = name[0].text + " - " + name[1].text
-        display.lcd_display_string(x, 1)
-        #display.lcd_display_string(name[0].text,2)
-        display.long_string(display, y, 2)
-        print(local,"-",visitante,"time: ",t)
+
+        # Extract names 
+        local = name[0].text
+        visitante = name[1].text
+
+        # Extract scores
+        local_score = score[0].text
+        visitante_score = score[1].timer_txt
+
+        # Extract and parse time
+        timer_txt = timer[0].text
+        time = searchForDigits(timer_txt)
+       
+        score_str = local_score + "-" + visitante_score + "     minute: " + str(time)
+        names_str = local + " - " + visitante
+
+        # Display strings 
+        display.lcd_display_string(score_str, 1)
+        display.long_string(names_str, 2)
+        
+        print(local,"-",visitante,"  minute: ",time)
         time.sleep(15)
 
-except KeyboardInterrupt: # If there is a KeyboardInterrupt (when you press ctrl+c), exit the program and cleanup
+except KeyboardInterrupt:   
     print("Cleaning up!")
     display.lcd_clear()
